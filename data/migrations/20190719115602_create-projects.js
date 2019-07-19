8e89d797777e7555b7a83e3e239998b567ce0302
lowlyHero <e.nunn1009@gmail.com>
2019-07-19 12:39:14 -0400

    project routercomplete

--- data/migrations/20190719115602_create-projects.js
+++ data/migrations/20190719115602_create-projects.js
@@ -1,8 +1,13 @@
 
-exports.up = function(knex) {
-  
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('projects', table => {
+    table.increments().unique();
+    table.text('name', 128);
+    table.text('description');
+    table.boolean('is_complete');
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('projects');
 };
--- data/migrations/20190719115703_create-actions.js
+++ data/migrations/20190719115703_create-actions.js
@@ -1,8 +1,13 @@
 
-exports.up = function(knex) {
-  
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('actions', table => {
+    table.increments().unique();
+    table.text('description');
+    table.text('notes');
+    table.boolean('is_complete');
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('actions');
 };
