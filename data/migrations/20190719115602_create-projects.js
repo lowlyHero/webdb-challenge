1f4b8c3fc33e6ed9d91b8ac70d90a9630d688086
lowlyHero <e.nunn1009@gmail.com>
2019-07-19 13:05:12 -0400

    Trying to get projects table set up to show actions in get request, other than that I am done

--- data/migrations/20190719115602_create-projects.js
+++ data/migrations/20190719115602_create-projects.js
@@ -2,8 +2,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('projects', table => {
     table.increments().unique();
-    table.text('name', 128);
-    table.text('description');
+    table.string('name');
+    table.string('description');
     table.boolean('is_complete');
   });
 };
--- data/migrations/20190719115703_create-actions.js
+++ data/migrations/20190719115703_create-actions.js
@@ -2,8 +2,9 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('actions', table => {
     table.increments().unique();
-    table.text('description');
-    table.text('notes');
+    table.foreign('project_id').notNullable().references('projects.id').inTable('projects');
+    table.string('description');
+    table.string('notes');
     table.boolean('is_complete');
   });
 };
