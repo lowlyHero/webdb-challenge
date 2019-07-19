27e617d1202d482a7b08b12ba226d4c361bad7b7
lowlyHero <e.nunn1009@gmail.com>
2019-07-19 12:50:59 -0400

    routers completed. dummy data added to projects with insomnia

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express'); 
 
-//Import Routers Here
+const projectRouter = require('./routes/projectRouter');
+const actionRouter = require('./routes/actionRouter');
 
 const server = express();
 
 server.use(express.json());
 
-// server.use('/projects', projectRouter);
-// server.use('/actions', actionRouter);
+server.use('/projects', projectRouter);
+server.use('/actions', actionRouter);
 
 server.get('/', (req, res) => {
     res.send('<h1>I\'m not insane</h1>');
